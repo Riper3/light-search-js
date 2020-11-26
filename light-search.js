ec140c6885a37d8ba1a76b17aa275f48801a4460
Javier Gonzalez Llamas <lastoner4@gmail.com>
2020-11-26 20:02:34 +0000

    Added the option to make a custom msg

--- light-search.js
+++ light-search.js
@@ -11,5 +11,30 @@ const lightSearch = (input, elements, options = {}) => {
           element.style.display = options.hide ? options.hide : 'none';
       }
     }
+
+    if (options.noresultsmsg && options.parent) {
+
+      const errormsg = document.querySelector('.light-search-noresults');
+
+      if (!result) {
+        const msgtag = options.msgtag ? options.msgtag : 'p';
+        const msgtext = options.msgtext ? options.msgtext : 'No results found';
+
+        const msgelement = document.createElement(msgtag);
+              msgelement.innerHTML = msgtext;
+              msgelement.classList.add('light-search-noresults');
+
+        if (!errormsg) {
+          options.parent.appendChild(msgelement);
+        }
+
+      } else {
+
+        if (errormsg) {
+          errormsg.remove();
+        }
+        
+      }
+    }
   });
 }
